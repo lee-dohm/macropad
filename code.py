7690a14aa603494b65fe0d8b1378b0a8d48898d5
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2021-07-29 09:42:36 -0700

    Simplify the code and create first mostly-complete mode

--- code.py
+++ code.py
@@ -2,5 +2,34 @@ from adafruit_macropad import MacroPad
 
 macropad = MacroPad()
 
+text_lines = macropad.display_text(title = '* lee-dohm Macropad *')
+
+def send_string(text):
+    macropad.keyboard_layout.write(text)
+
+DEFAULTS = {
+    'mode_text': '----- RimWorld  -----',
+    'keys': [
+        '1',
+        '2',
+        '3',
+        "'",
+        ',',
+        '.',
+        'a',
+        'o',
+        'e',
+        None,
+        None,
+        ' '
+    ]
+}
+
+text_lines[0].text = DEFAULTS['mode_text']
+text_lines.show()
+
 while True:
-    pass
+    key_event = macropad.keys.events.get()
+
+    if key_event and key_event.pressed:
+        send_string(DEFAULTS['keys'][key_event.key_number])
